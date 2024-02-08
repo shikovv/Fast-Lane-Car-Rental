a4feef448d62612e665d148c33454563ac29755a
shikovv <stefanshikov@abv.bg>
2024-02-08 23:33:49 +0200

    Entities 3.0

---
+++ CarRental/Data/Domain/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRental.Data.Domain
+{
+    public class Rental
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+        public double Deposit { get; set; }
+        [ForeignKey(nameof(Car))]
+        public virtual Car Car { get; set; } = null!;
+    }
+}
