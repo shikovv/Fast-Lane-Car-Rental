4c25d0ce47303f15fe383a9e754673b13fff225a
shikovv <137146520+shikovv@users.noreply.github.com>
2024-02-08 23:34:28 -1000

    Entities 2.1

---
+++ CarRental/Data/Domain/UserRental.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRental.Data.Domain
+{
+    public class UserRental
+    {
+        [ForeignKey(nameof(Rental))]
+        public Guid RentalId { get; set; }
+        public virtual Rental Rental { get; set; } = null!;
+    }
+}
