ba767d61e163e20c82c3a805423b0be7f910fd8f
shikovv <137146520+shikovv@users.noreply.github.com>
2024-02-19 11:37:22 +0200

    awerg

---
+++ CarRental/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string UserName {  get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ CarRental/Models/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 3)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(15, MinimumLength = 3)]
+        public string Surname { get; set; } = null!;
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        [StringLength(60, MinimumLength = 10)]
+        public string EmailAddress { get; set; } = null!;
+        [Required]
+        [StringLength(20, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
