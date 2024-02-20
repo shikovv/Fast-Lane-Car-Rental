91d44a4e6f4a1558f30f0adc2cbac11248bd56e1
shikovv <137146520+shikovv@users.noreply.github.com>
2024-02-20 09:38:49 +0200

    identity work

---
+++ CarRental/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using CarRental.Data.Domain;
+using CarRental.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        public UserController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if(User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index");
+            }
+            var model = new RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new ApplicationUser()
+            {
+                FirstName=model.FirstName,
+                LastName=model.Surname,
+                Email = model.EmailAddress,
+                UserName = model.UserName
+            };
+            var result= await userManager.CreateAsync(user,model.ConfirmPassword);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            foreach(var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if(User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index");
+            }
+            var model=new LoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) 
+            { 
+                return View(model);
+            }
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+        }
+    }
+}
--- CarRental/Models/RegisterViewModel.cs
+++ CarRental/Models/RegisterViewModel.cs
@@ -23,6 +23,6 @@ namespace CarRental.Models
         public string Password { get; set; } = null!;
         [Compare(nameof(Password))]
         [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;    
     }
 }
