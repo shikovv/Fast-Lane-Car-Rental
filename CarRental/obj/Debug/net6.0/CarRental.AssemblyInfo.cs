bdb02d4fed35e2b51cd46ab608e82047f43804ae
shikovv <stefanshikov@abv.bg>
2024-03-14 00:52:31 +0200

    login and register views ready

--- CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9988a4e9bad3bc0eda11981fb029a9b90693783c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
