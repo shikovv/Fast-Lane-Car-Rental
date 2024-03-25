56af4bf8f10e18cdfd1152f333caf7bb9f570638
shikovv <137146520+shikovv@users.noreply.github.com>
2024-03-25 10:30:33 +0200

    created view for start page

--- CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9661bd011220a5e5c20d620d4b5d35e746fd9a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6fcb277bb6ae9b7140656b1aa9c68c8b5dc5240")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
