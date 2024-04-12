6a5fdd399f60aa2a3447e3fc0fcf12028ea94bd2
shikovv <stefanshikov@abv.bg>
2024-04-12 21:21:19 +0300

    project almost ready-just needs some styling and tests

--- CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258ae6dd547e425cf97c9bb52ed74eaebd453edb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accdf46654d1d8be37d93d9ff73d9ea8f32b5812")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
